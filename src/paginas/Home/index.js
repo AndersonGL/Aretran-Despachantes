c0c39c238ccb80e73e3db1b5d7e2bb8782614d11
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2024-02-03 16:34:08 -0300

    Update index.js

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -36,7 +36,7 @@ export default function Home() {
                 <h2>CLIENTES SATISFEITOS</h2>
                 <span>Lorem Ipsum</span>
                 <p>
-                Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
                 </p>
             </div>
             <img src="assets/motorista.png" alt="foto de uma cnh" />
