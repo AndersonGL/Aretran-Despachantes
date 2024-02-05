98eb6eee0b9370e3ddd1fbc100964ae11c260c67
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2024-02-04 23:24:58 -0300

    Update index.js

--- src/paginas/Sobre/index.js
+++ src/paginas/Sobre/index.js
@@ -21,11 +21,11 @@ export default function Sobre() {
 
         <section className="container-secao-sobre-nos">
           <div className="limitar-secao">
-            <h2>Sobre Nós</h2><br>
+            <h2>Sobre Nós</h2>
             <span>Conquistar um novo cliente é sempre bom. Mas manter os de longa data é melhor ainda.</span>
             <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            </p>      
+           Desde 2007 atuando e transformando a vida de nossos clientes.
+    </p>
           </div>
         </section>
 
