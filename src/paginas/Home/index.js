ba75be563d7065b35613e12f1d68b41bdd234fd0
Anderson GL <lignelli77@gmail.com>
2024-02-10 01:19:16 -0300

    ok

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -53,6 +53,7 @@ export default function Home() {
                         </p>
                     </div>
                 </section>
+               
             </main>
 
             < Footer />
