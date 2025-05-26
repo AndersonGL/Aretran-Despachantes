be31e216ca274ec8218ae6b70dabb17e973045d7
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-05-26 20:52:54 -0300

    update

--- src/paginas/Sobre/index.js
+++ src/paginas/Sobre/index.js
@@ -1,18 +1,14 @@
 import Header from "../../componentes/Header";
 import Footer from "../../componentes/Footer";
 
-
-import './style.css';
+import "./style.css";
 
 export default function Sobre() {
-
   return (
     <div>
-
       <Header />
 
       <main>
-
         <section className="secao-banner-sobre">
           <div className="titulo-banner-sobre">
             <h1>O ARETRAN</h1>
@@ -22,25 +18,28 @@ export default function Sobre() {
         <section className="container-secao-sobre-nos">
           <div className="limitar-secao">
             <h2>Sobre Nós</h2>
-            
-     <p>"Desde 2007 atuando e transformando a vida de nossos clientes."</p>
-  <p>"Conquistar um novo cliente é sempre bom. Mas manter os de longa data é melhor ainda".</p>
-      <p>Parcelamos até em 12x</p>
 
-      <img src="assets/cartoes.webp" alt="pagamentos"/>
-     
+            <p class="sobre">
+              "Desde 2007 atuando e transformando a vida de nossos clientes."
+            </p>
+            <p class="sobre">
+              "Conquistar um novo cliente é sempre bom. Mas manter os de longa
+              data é melhor ainda".
+            </p>
+            <p class="sobre">Parcelamos até em 12x</p>
 
+            <img src="assets/cartoes.webp" alt="pagamentos" />
           </div>
         </section>
 
         <section>
-                <a href='https://wa.me/5511974559801?text=Ol%C3%A1%21%21%21+aretran+quero+tirar+uma+d%C3%BAvida..."'><button className="whatsapp"></button></a>
+          <a href='https://wa.me/5511974559801?text=Ol%C3%A1%21%21%21+aretran+quero+tirar+uma+d%C3%BAvida..."'>
+            <button className="whatsapp"></button>
+          </a>
         </section>
       </main>
 
-
       <Footer />
     </div>
-  )
-
+  );
 }
