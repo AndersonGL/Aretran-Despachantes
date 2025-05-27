c2c1dc1354c6416518cd8b4957edc8a3a5021eaf
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-05-27 00:16:00 -0300

    update

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -1,59 +1,41 @@
 import Header from "../../componentes/Header";
 import Footer from "../../componentes/Footer";
 
-import './style.css'
-
+import "./style.css";
 
 export default function Home() {
+  return (
+    <div>
+      <Header />
 
-    return (
+      <main>
+        <section className="secao-banner">
+          <div className="texto-banner">
+            <h1>ASSESSORIA & DESPACHANTES ASSOCIADOS TESTE</h1>
+          </div>
+        </section>
 
-        <div>
+        <section className="secao-nossos-clientes">
+          <img src="assets/motorista.webp" alt="foto motorista" />
+          <div className="texto-secao-nossos-clientes">
+            <h2>CLIENTES SATISFEITOS</h2>
+            <span>Gabriel Gomes</span>
+            <p>
+              " Gostaria de agradecer o Aretran pelo comprometimento e entrega
+              sou motorista de aplicativo precisava adicionar o EAR e renovar
+              minha CNH estou muito contente".
+            </p>
+          </div>
+        </section>
 
+        <section>
+          <a href='https://wa.me/5511974559801?text=Ol%C3%A1%21%21%21+aretran+quero+tirar+uma+d%C3%BAvida..."'>
+            <button className="whatsapp"></button>
+          </a>
+        </section>
+      </main>
 
-            <Header />
-
-
-
-            <main>
-                <section className="secao-banner">
-                    <div className="texto-banner">
-                        <h1>  ASSESSORIA & DESPACHANTES ASSOCIADOS </h1>
-                    </div>
-                </section>
-
-
-
-                <section className="secao-nossos-clientes">
-                    <img src="assets/motorista.webp" alt="foto motorista" />
-                    <div className="texto-secao-nossos-clientes">
-                        <h2>CLIENTES SATISFEITOS</h2>
-                        <span>Gabriel Gomes</span>
-                        <p>
-                            " Gostaria de agradecer o Aretran pelo comprometimento e entrega sou motorista de  aplicativo precisava adicionar o EAR e renovar minha CNH estou muito contente".
-                        </p>
-                    </div>
-
-                </section>
-
-            
-
-              
-
-                <section>
-                <a href='https://wa.me/5511974559801?text=Ol%C3%A1%21%21%21+aretran+quero+tirar+uma+d%C3%BAvida..."'><button className="whatsapp"></button></a>
-                </section>
-
-            </main>
-
-            <Footer />
-
-
-        </div>
-
-    )
-
+      <Footer />
+    </div>
+  );
 }
-
-
-
