a0181a75a30f3aff17d3966141ba4650e8bc5dae
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-09-09 17:26:44 -0300

    update

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <main>
         <section className="secao-banner">
           <div className="texto-banner">
-            <h1> INTERMEDIAÇÕES & ASSESSORIA DOCUMENTAL</h1>
+            <h1>INTERMEDIAÇÕES & ASSESSORIA DOCUMENTAL</h1>
           </div>
         </section>
 
