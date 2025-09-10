145d8e49d30d1423da7663808a25a5066e83cde0
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-09-09 21:02:33 -0300

    testando

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders main banner heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/INTERMEDIAÇÕES & ASSESSORIA DOCUMENTAL/i);
+  expect(headingElement).toBeInTheDocument();
 });
